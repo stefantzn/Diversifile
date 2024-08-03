2a67071fbf2e1feb43cb1cab49832ada2957a046
Stefan Tuczynski <tuczynskis@gmail.com>
2024-08-03 07:50:46 -0400

    authentication

---
+++ frontend/app/_document.js
@@ -0,0 +1,18 @@
+// pages/_document.js
+
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Add your favicon here */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,12 +1,20 @@
+'use client';
+
 import Link from 'next/link';
+import Image from 'next/image'; 
+import logo from '../public/logo.png';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 const Navbar = () => {
+
+  const { user } = useUser();
+
   return (
     <nav className="p-6 w-full">
       <div className="flex items-center justify-between">
-        <div className="text-white text-4xl font-bold pl-4">
-          <Link href="/" className="hover text-gray-300">
-            D
+        <div className="pl-4">
+          <Link href="/">
+            <Image src={logo} alt="Logo" width={40} height={40} /> 
           </Link>
         </div>
 
@@ -22,11 +30,15 @@ const Navbar = () => {
               About
             </Link>
           </li>
-          <li>
-            <Link href="/Login" className="text-white hover:text-gray-300">
-              Login
-            </Link>
-          </li>
+          {!user ? (
+            <li>
+              <a className="text-white" href="/api/auth/login">Login</a>
+            </li>
+          ) : (
+            <li>
+              <a className="text-white" href="/api/auth/logout">Logout</a>
+            </li>
+          )}
         </ul>
       </div>
     </nav>
